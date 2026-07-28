3b4bff6eede2aef559645167b0489d043a110831
Alexa Deby <alexadeby05@gmail.com>
2026-07-28 18:36:04 +0700

    perubahan foter

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,13 +10,13 @@
         <!-- Nama Desa & Kabupaten -->
         <div class="leading-snug">
             <h2 class="text-lg font-bold text-gray-900">Desa Jatisari</h2>
-            <p class="text-sm text-gray-700">Kabupaten Malang, Jawa Timur</p>
+            <p class="text-sm text-gray-700">Kecamatan Tajinan, Kabupaten Malang, Jawa Timur</p>
         </div>
 
         <!-- Alamat & Kontak -->
         <div class="text-sm text-gray-700 space-y-1">
             <p class="font-bold text-gray-900">Kantor Desa Jatisari :</p>
-            <p>Jl. Contoh Alamat No. 1, Jatisari, Kec. Contoh, Kabupaten Malang, Jawa Timur 65100, Indonesia</p>
+            <p>Jl. K.H.Salim No. 1, Krajan, Jatisari, Kec. Contoh, Kabupaten Malang, Jawa Timur 65100, Indonesia</p>
             <p>
                 Email : <a href="mailto:desa.jatisari@malangkab.go.id" class="text-blue-600 hover:underline">desa.jatisari@malangkab.go.id</a> 
                 | No. Telp : 0341xxxxxx
@@ -27,6 +27,9 @@
         <div class="text-sm space-y-1 pt-1">
             <p class="font-semibold text-gray-800 hover:underline cursor-pointer">
                 Ikuti Kami di Media Sosial Lainnya
+                Instagram : 
+                Facebook  :
+                Youtube   :
             </p>
             <p>
                 <a href="#" class="text-gray-700 hover:underline">Kebijakan Privasi</a>
@@ -35,7 +38,7 @@
 
         <!-- Copyright -->
         <div class="pt-2 text-sm text-gray-700">
-            <p>© {{ date('Y') }} Jatisari - Website ini dibangun dan dikelola oleh <strong class="font-bold text-gray-900">PSID Jatisari</strong></p>
+            <p>© {{ date('Y') }} Jatisari - Website ini dibangun oleh Himpunan Departemen Teknik Elektro dan dan dikelola oleh <strong class="font-bold text-gray-900">PSID Jatisari</strong></p>
         </div>
 
     </div>
